4b2e8c5fff0b984732a14733dcaefe907316765f
aj <ali.becerra1224@gmail.com>
2023-01-18 15:49:00 -0800

    finished part 7

--- sphere.h
+++ sphere.h
@@ -7,7 +7,7 @@
 class sphere : public hittable {
 public:
   sphere() {}
-  sphere(piont3 cen, double r) : center(cen), radius(r){};
+  sphere(point3 cen, double r) : center(cen), radius(r){};
 
   virtual bool hit(const ray &r, double t_min, double t_max,
                    hit_record &rec) const override;
@@ -40,7 +40,10 @@ bool sphere::hit(const ray &r, double t_min, double t_max,
 
   rec.t = root;
   rec.p = r.at(rec.t);
-  rec.normal = (rec.p - center) / radius;
+  vec3 outward_normal = (rec.p - center)/radius;
+  rec.set_face_normal(r, outward_normal);
+
+
 
   return true;
 }
