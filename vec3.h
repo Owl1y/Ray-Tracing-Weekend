38dac9dbc030106f77b5f85d30eca3ea5bec7535
aj <ali.becerra1224@gmail.com>
2023-01-19 16:55:41 -0800

    i got it to render a matte cirlce but its darker than the sample photo

--- vec3.h
+++ vec3.h
@@ -1,6 +1,7 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include "rtweekend.h"
 #include <cmath>
 #include <iostream>
 #include <ostream>
@@ -11,41 +12,49 @@ class vec3{
   public:
     vec3() : e{0,0,0}{}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2}{}
-
+    
     double x() const {return e[0];}
     double y() const {return e[1];}
     double z() const {return e[2];}
-
+    
     vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]);}
     double operator[](int i) const {return e[i];}
     double& operator[](int i){return e[i];}
-
+    
     vec3&operator+=(const vec3 &v){
       e[0] += v.e[0];
       e[1] += v.e[1];
       e[2] += v.e[2];
       return *this;
     }
-
+    
     vec3& operator*=(const double t){
       e[0] *= t;
       e[1] *= t;
       e[2] *= t;
       return *this;
     }
-
+    
     vec3& operator/=(const double t){
       return *this *= 1/t;
     }
-
+    
     double length() const{
       return sqrt(length_squared());
     }
-
+    
     double length_squared() const{
       return e[0]*e[0]+e[1]*e[1]+e[2]*e[2];
     }
-  
+    inline static vec3 random(){
+      return vec3(random_double(), random_double(), random_double());
+    }
+    
+    inline static vec3 random(double min, double max){
+      return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+    }
+
+
   public:
     double e[3];
 
@@ -101,5 +110,21 @@ inline vec3 cross(const vec3 &u, const vec3 &v){
 inline vec3 unit_vector(vec3 v){
   return v/v.length();
 }
+
+vec3 random_in_unit_sphere(){
+  while(true){
+    auto p = vec3::random(-1,1);
+    if (p.length_squared() >= 1){
+      continue;
+    } 
+    return p;
+  }
+}
+
+
+
+
+
+
 
 #endif
